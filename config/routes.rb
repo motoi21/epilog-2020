9e103f4f2b9449a12eeaac23ee078a58686ff29f
motoi21 <m250609@gmail.com>
2020-11-05 19:46:29 +0900

    新規プロフィール作成のビューファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+  end
   root to: "drawings#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
