fe830a6dd83c4fe4ff3550c4db5dc8755a3d7919
motoi21 <m250609@gmail.com>
2020-11-05 15:56:31 +0900

    usersテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'drawings/index'
-  
+  devise_for :users
+  root to: "drawings#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
