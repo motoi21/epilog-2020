b5b25f0ecb29b5277d3ac0e0a0acd3e148ff78d2
motoi21 <m250609@gmail.com>
2020-11-06 17:43:05 +0900

    passwordに関するバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   has_one :profile
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
