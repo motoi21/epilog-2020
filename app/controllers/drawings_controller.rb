4c314a722738557732ec1dee9058667312a5c369
motoi21 <m250609@gmail.com>
2020-11-08 16:17:35 +0900

    drawingsコントローラーに処理を記述

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,4 +1,22 @@
 class DrawingsController < ApplicationController
   def index
   end
+
+  def new
+    @drawing = Drawing.new
+  end
+
+  def create
+    @drawing = Drawing.new(drawing_params)
+    if @drawing.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def drawing_params
+    params.require(:drawing).permit(:image, :title, :description, :production_date, :category_id, :genre_id, :price, :shipping_method_id).merge(user_id: current_user.id)
+  end
 end
