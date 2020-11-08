e1b59d819e7f35679ab67f23b7a5a0d07ce482f7
motoi21 <m250609@gmail.com>
2020-11-08 15:37:14 +0900

    ActiveStorageの導入

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,8 +1,10 @@
 class Drawing < ApplicationRecord
   belongs_to :user
-
+  has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :genre
   belongs_to_active_hash :shipping_method
+
 end
