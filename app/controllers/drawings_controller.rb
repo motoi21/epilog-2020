5942c5af60c41606846b981af0e79140e1609c2e
motoi21 <m250609@gmail.com>
2020-11-10 18:34:18 +0900

    index.erbを編集
    categoryやgenreごとにdrawingが一覧表示されるようにビューファイルを編集

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -4,10 +4,10 @@ class DrawingsController < ApplicationController
   def index
     @drawings_with_price = Drawing.where.not(price: nil)
     @drawings_analog = Drawing.where(category_id: 3)
-    @drawing_illustration = Drawing.where(genre_id: 2)
-    @drawing_manga = Drawing.where(genre_id: 3)
-    @drawing_portrait = Drawing.where(genre_id: 9)
-    @drawing_poster = Drawing.where(genre_id: 4)
+    @drawings_illustration = Drawing.where(genre_id: 2)
+    @drawings_manga = Drawing.where(genre_id: 3)
+    @drawings_portrait = Drawing.where(genre_id: 9)
+    @drawings_poster = Drawing.where(genre_id: 4)
   end
 
   def new
