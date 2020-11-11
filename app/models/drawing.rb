0f11264c43618b514ae0633159750d45aabfa107
motoi21 <m250609@gmail.com>
2020-11-11 20:02:28 +0900

    purchaseモデルを作成しアソシエーションを設定

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,6 +1,7 @@
 class Drawing < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_one :purchase
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
