0e5b5e3690685e6c30dc913fdedb86307d92b621
motoi21 <m250609@gmail.com>
2020-11-12 20:34:42 +0900

    fix: purchase_shippingモデルの記述の訂正

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -13,6 +13,6 @@ class PurchaseShipping
 
   def save
     purchase = Purchase.create(user_id: user_id, drawing_id: drawing_id)
-    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase_id)
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
