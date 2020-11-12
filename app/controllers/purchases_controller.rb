9f46314a9ee4b2244099a77ebe5c0ca403fa80ed
motoi21 <m250609@gmail.com>
2020-11-12 19:19:14 +0900

    ストロングパラメーターの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,12 @@
 class PurchasesController < ApplicationController
   def new
+    @drawing = Drawing.find(params[:drawing_id])
     @purchase_shipping = PurchaseShipping.new
   end
 
   def create
-    @purchase_shipping = PurchaseShipping.new
+    @drawing = Drawing.find(params[:drawing_id])
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
     if @purchase_shipping.valid?
       @purchase_shipping.save
       redirect_to root_path
@@ -13,4 +15,8 @@ class PurchasesController < ApplicationController
     end
   end
 
+  private
+  def purchase_params
+    params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, drawing_id: params[:drawing_id])
+  end
 end
