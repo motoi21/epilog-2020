c0da58ff8df26c6169e3097bc256495d47ea827c
motoi21 <m250609@gmail.com>
2020-11-12 18:37:10 +0900

    ActiveModelにバリデーションを設定

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,6 +1,15 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :drawing_id, :postal_code, :prefecture, :city, :address, :building, :phone_number, :purchase_id 
+  attr_accessor :user_id, :drawing_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number 
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly'}
+    validates :city
+    validates :address
+    validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+end
 
   
-end
