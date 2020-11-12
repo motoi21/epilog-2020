80df1cd39c8491a4ba43d81f8236382ffd2e1f83
motoi21 <m250609@gmail.com>
2020-11-12 19:24:34 +0900

    繰り返しの記述に関するリファクタリング
    繰り返しの記述をprivateメソッドに定義しbefore_actionで呼び出した

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,11 @@
 class PurchasesController < ApplicationController
+  before_action :set_drawing, only: [:new, :create]
+
   def new
-    @drawing = Drawing.find(params[:drawing_id])
     @purchase_shipping = PurchaseShipping.new
   end
 
   def create
-    @drawing = Drawing.find(params[:drawing_id])
     @purchase_shipping = PurchaseShipping.new(purchase_params)
     if @purchase_shipping.valid?
       @purchase_shipping.save
@@ -18,5 +18,9 @@ class PurchasesController < ApplicationController
   private
   def purchase_params
     params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, drawing_id: params[:drawing_id])
+  end
+
+  def set_drawing
+    @drawing = Drawing.find(params[:drawing_id])
   end
 end
