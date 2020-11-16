2ff7dd8fa124f823206fb624ff9fc41b48a3366b
motoi21 <m250609@gmail.com>
2020-11-16 22:08:35 +0900

    ユーザー名にリンク先のパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     post 'profiles', to: 'users/registrations#create_profile'
   end
   root to: "drawings#index"
-  resources :users, onlu:[:show]
+  resources :users, only: [:show]
   resources :drawings do
     resources :purchases, only: [:new, :create]
   end
