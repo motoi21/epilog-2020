a60255da3069fd12ea3f9b6aebefbe36004c043e
motoi21 <m250609@gmail.com>
2020-11-21 15:13:00 +0900

    本人以外のユーザーがprofieを編集できないようにuser contorollerに記述

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!, only:[:edit, :update]
   before_action :set_user_profile, only:[:edit, :update]
-    
+  before_action :move_to_show, only: [:edit, :update]
+  
   def edit
   end
 
@@ -21,4 +23,11 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:id])
     @profile = Profile.find_by(user_id: current_user.id)
   end
+
+  def move_to_show
+    unless user_signed_in? && current_user.profile.id == @user.profile.id
+      redirect_to user_path(@user.id)
+    end
+  end
+
 end
