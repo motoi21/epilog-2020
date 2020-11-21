6391d9193e9eae8987b6b02ae0c299a0885d807b
motoi21 <m250609@gmail.com>
2020-11-21 14:40:42 +0900

    繰り返しの記述のリファクタリング
    繰り返しの記述をset_userと定義し、beforeアクションで呼び出す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
     
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(current_user.id)
     else 
@@ -20,5 +19,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
-  end  
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
