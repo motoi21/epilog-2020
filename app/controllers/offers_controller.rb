4b66f0f314efc933948615bef07dedb1866536b0
motoi21 <m250609@gmail.com>
2020-11-23 14:02:35 +0900

    offers controllerにcreateアクションの処理を記述

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,4 +7,14 @@ class OffersController < ApplicationController
     @user = User.find(params[:user_id])
     @offer = Offer.new
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @offer = Offer.new
+    if @order.save
+      redirect_to user_offers_path(@user.id)
+    else
+      render :new
+    end
+  end
 end
