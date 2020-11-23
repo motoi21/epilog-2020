417ece41f8a63483a73edc583cfc21007ca71521
motoi21 <m250609@gmail.com>
2020-11-23 16:15:51 +0900

    offerモデルにバリデーションを設定

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,4 +4,15 @@ class Offer < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :genre
   belongs_to_active_hash :shipping_method
+
+  with_options presence: true do
+    validates :order_name
+    validates :detail
+    validates :deadline
+  end
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :genre_id
+  end
+  validates :budget, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 end
